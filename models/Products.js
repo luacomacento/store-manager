1a1b5512627001ff57405ef64fce3f1e881685ab
Luá <luacomacento@gmail.com>
2022-06-29 15:15:57 -0300

    fix: remove variável de ambiente no model

--- models/Products.js
+++ models/Products.js
@@ -1,16 +1,16 @@
 const db = require('./db');
 
-const database = process.env.MYSQL_DATABASE;
+// const database = process.env.MYSQL_DATABASE;
 
 const Products = {
   getAll: async () => {
-    const sql = `SELECT * FROM ${database}.products`;
+    const sql = 'SELECT * FROM StoreManager.products';
     const [rows] = await db.query(sql);
     return rows;
   },
 
   getById: async (id) => {
-    const sql = `SELECT * FROM ${database}.products WHERE id = ?`;
+    const sql = 'SELECT * FROM StoreManager.products WHERE id = ?';
     const [[item]] = await db.query(sql, [id]);
     return item;
   },
