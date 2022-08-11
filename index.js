9d5511224bea5fa4c583361e3d9d420253505925
luacomacento <luacomacento@gmail.com>
2022-08-11 19:48:49 -0300

    refactor: adds PORT constant to app.listen

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
 
-app.listen(process.env.PORT || 3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT || 3000}`);
 });
